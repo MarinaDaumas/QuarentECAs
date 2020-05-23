1f30b8ad204e54199787e3d360d96720c7d4e223
Marina Daumas <marina.daumas@poli.ufrj.br>
2020-05-23 14:35:50 -0300

    nao sei oq mudou

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from banco import adicionar_pedido, buscar_clientes_contaminados
 
 
 def novo_pedido():
-
+    """
+    pedido = {"data" = '', entregador:[nome, cpf], cliente:[nome, email, telefone]}
+    """
     # ler JSON no caso de cada novo pedido ser um json
     with open ("pedido.json", 'r') as ped:
         pedido = json.load(ped)
@@ -12,10 +14,14 @@ def novo_pedido():
     adicionar_pedido(pedido)
 
 def nova_contaminacao():
-
+    """
+    aviso = {"nome": '', "cpf": ''}
+    """
     #ler JSON entregador contaminado
     with open ("aviso.json", 'r') as avi:
         aviso = json.load(avi)
+
+    expostos = buscar_clientes_contaminados(aviso[cpf])
 
 
 
