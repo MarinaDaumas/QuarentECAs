66e7cf97ee67a32b60389236b777ac61fe8374d1
Ney Barbosa <rafaelneyney@gmail.com>
2020-05-24 10:39:44 -0300

    banco de dados add e funcoes ajeitadas

--- testando.py
+++ testando.py
@@ -1,21 +1,21 @@
 from banco import *
-from datetime import date, time, timedelta
-
+import datetime 
+'''
 def limpar():
 
     # data AAAA-MM-DD 
     hoje = date.today()
-    tempo = timedelta(days=-1)
+    tempo = timedelta(days=15)
     outro_dia = hoje - tempo
 
     limpar_pedidos_antigos(outro_dia)
-
+'''
 if __name__ == "__main__":
     criar_tabelas()
-    '''
-    hora = datetime.today()
-    pedido = {"data" : hora, "entregador":['robson', 175855], "cliente":['jose', 'ze@gmail.com', 3654]}
+    
+    hora = datetime.date.today()
+    pedido = {"data" : hora, "entregador":['robson', 8520741], "cliente":['CLodoaldo Silva', 'clo.clo@gmail.com', 40028922]}
     adicionar_pedido(pedido)
-    '''
+    
     #buscar_clientes_contaminados(175855)
-    limpar()
+    #limpar()
