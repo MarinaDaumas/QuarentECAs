abb7dc304f339050094c116ef6387c57f16afd7c
heitor-andrade <heitor_ndrd@poli.ufrj.br>
2020-05-24 12:51:03 -0300

    depurando o fluxo da página

--- site.py
+++ site.py
@@ -8,10 +8,10 @@ def quem_somos():
     return render_template("home.html")
 
 
-@app.route("/contatenos")
+@app.route("/contato")
 def contate_nos():
 	return render_template("contato.html")
-
+    
 @app.route("/obrigado", methods=["POST", "GET"])
 def obrigado():
     if request.method == "POST":
